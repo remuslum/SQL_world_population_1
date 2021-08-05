ee11a8e4fe46153d386f80feb93f355b67b13f00
remuslum <57741494+remuslum@users.noreply.github.com>
2021-08-05 14:07:51 +0800

    Add files via upload

---
+++ population_queries.sql
@@ -0,0 +1,17 @@
+-- This is the first query:
+
+SELECT DISTINCT year from population_years;
+
+-- Add your additional queries below:
+-- This is the first query:
+
+SELECT DISTINCT year from population_years;
+
+-- Add your additional queries below:
+
+SELECT COUNT(DISTINCT year) AS years_covered from population_years; 
+SELECT MAX(population) AS largest_population from population_years WHERE country = "Gabon" ;
+SELECT population FROM population_years WHERE year = 2005 ORDER BY population LIMIT 10 ;
+SELECT DISTINCT country FROM population_years WHERE population > 100 ; 
+SELECT DISTINCT country FROM population_years WHERE country LIKE '%Islands%' ; 
+SELECT population - (SELECT population FROM population_years WHERE year = 2000 AND country = 'Indonesia') AS Difference FROM population_years WHERE year = 2010 AND country = 'Indonesia' ; 
